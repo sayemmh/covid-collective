367a048bd3863ddf5d334ac9cd6e1635b9cee258
Sayem Hoque <sayemmh@gmail.com>
2020-04-11 11:09:04 -0400

    change image

--- src/components/body/imageblob.js
+++ src/components/body/imageblob.js
@@ -3,14 +3,9 @@ import styled from "styled-components";
 import { mobileScreenSize } from "styleguide/breakpoints";
 
 const StyledBlurb = styled.p`
-  @media screen and (min-width: ${mobileScreenSize}) {
-    max-width: 650px;
-  }
   display: block;
-  margin-left: auto;
-  margin-right: auto;
   height: 320px;
-  width: 100%;
+  max-width: 100%;
   background-repeat: no-repeat;
 
   background-image: url(${props => props.imageUrl});
